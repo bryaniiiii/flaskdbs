c8f1fe6366e527c99995f085d941b0494a9e57d7
Vena Chandra Salim <venasalim@Venas-MacBook-Pro-2.local>
2022-12-03 13:53:16 +0800

    fixed module errors

--- main.py
+++ main.py
@@ -73,4 +73,4 @@ api.add_resource(Login, "/login")
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("$PORT", default=5000))
