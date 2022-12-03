b632960f5fbb87be4a00241aa358e39613b4c6d3
kanaxo <60850434+kanaxo@users.noreply.github.com>
2022-12-03 11:36:44 +0800

    create connection to mysql db

--- app/transact.py
+++ app/transact.py
@@ -1,12 +1,21 @@
 from flask import Flask, jsonify
 import os
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/bank'
+
+url = "mysql://root:@localhost/bank"
 
 
 @app.route('/')
 def index():
+    engine = create_engine(url, echo=TRUE)
+    connection = engine.connect()
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
+
 
 
 if __name__ == '__main__':
